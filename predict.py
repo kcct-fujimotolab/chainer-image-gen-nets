9d9b36a238966566bf06a879627c3f6c7d3b5982
Hayato Kawai <fohte.hk@gmail.com>
2016-08-22 16:44:14 +0900

    Initial commit

---
+++ predict.py
@@ -0,0 +1,52 @@
+import glob
+import os
+import re
+
+import chainer
+import chainer.functions as F
+import chainer.links as L
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from chainer import training
+from chainer.training import extensions
+from PIL import Image
+
+import dcgan
+
+matplotlib.use('Agg')
+
+
+if __name__ == '__main__':
+
+    n_z = 100
+    n_column = 4
+    n_row = 4
+    n_img = n_column * n_row
+
+    model_dir = 'result/model'
+    output_dir = 'result/test'
+
+    try:
+        os.makedirs(output_dir)
+    except:
+        pass
+
+    model_epochs = set(ep for ep in os.listdir(model_dir))
+    exists_epochs = set(re.search('(\d+).png', f).group(1)
+                        for f in os.listdir(output_dir) if not f.startswith('.'))
+    predict_epochs = model_epochs - exists_epochs
+
+    print(predict_epochs)
+    for epoch in predict_epochs:
+        gen = dcgan.Generator()
+        chainer.serializers.load_npz(
+            '{}/{}/dcgan_model_gen.npz'.format(model_dir, epoch), gen)
+        z = np.random.uniform(-1, 1, (n_img, n_z)).astype(np.float32)
+
+        y = gen(z, test=True)
+        for i, img in enumerate(y.data):
+            plt.subplot(n_row, n_column, i + 1)
+            plt.imshow(((img + 1) / 2).transpose(1, 2, 0))
+            plt.axis('off')
+        plt.savefig('{}/{}.png'.format(output_dir, epoch))
---
+++ train.py
@@ -0,0 +1,120 @@
+import argparse
+import glob
+import math
+import os
+
+import chainer
+import chainer.functions as F
+import chainer.links as L
+import matplotlib.pyplot as plt
+import numpy
+import progressbar
+from chainer import training
+from chainer.training import extensions
+from PIL import Image
+
+import dcgan
+
+
+def import_train_images(image_dir):
+    files = glob.glob('{}/*'.format(os.path.abspath(image_dir)))
+    dataset = numpy.empty((len(files), 3, 96, 96), dtype=numpy.float32)
+
+    for i, img_file in enumerate(files):
+        img = numpy.asarray(Image.open(img_file).convert(
+            'RGB')).astype(numpy.float32).transpose(2, 0, 1)
+        # 0..256 to -1..1
+        img = (img / (256 / 2)) - 1
+        dataset[i] = img
+
+    return dataset
+
+
+def random_indexes(n):
+    xs = numpy.arange(n)
+    numpy.random.shuffle(xs)
+    return xs
+
+
+xp = chainer.cuda.cupy
+
+if __name__ == '__main__':
+
+    batchsize = 100
+    n_epoch = 10000
+    n_z = 100
+    snapshot_interval = 10
+
+    model_dir = 'result/model'
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_dir', help='Images directory for training')
+    parser.add_argument('--gpu', '-g', type=int, default=-1)
+    args = parser.parse_args()
+
+    train = import_train_images(args.input_dir)
+    n_train = len(train)
+
+    gen = dcgan.Generator()
+    dis = dcgan.Discriminator()
+
+    if args.gpu >= 0:
+        chainer.cuda.get_device(args.gpu).use()
+        gen.to_gpu()
+        dis.to_gpu()
+
+    optimizer_gen = chainer.optimizers.Adam(alpha=0.0002, beta1=0.5)
+    optimizer_dis = chainer.optimizers.Adam(alpha=0.0002, beta1=0.5)
+    optimizer_gen.setup(gen)
+    optimizer_dis.setup(dis)
+    optimizer_gen.add_hook(chainer.optimizer.WeightDecay(0.00001))
+    optimizer_dis.add_hook(chainer.optimizer.WeightDecay(0.00001))
+
+    sum_loss_dis = numpy.float32(0)
+    sum_loss_gen = numpy.float32(0)
+
+    progress = progressbar.ProgressBar()
+    for epoch in progress(range(n_epoch)):
+        perm = random_indexes(n_train)
+        for i in range(0, n_train - (n_train % batchsize), batchsize):
+            z = chainer.Variable(
+                xp.random.uniform(-1, 1, (batchsize, n_z), dtype=numpy.float32))
+            y_gen = gen(z)
+            y_dis = dis(y_gen)
+            loss_gen = F.softmax_cross_entropy(
+                y_dis, chainer.Variable(xp.zeros(batchsize, dtype=numpy.int32)))
+            loss_dis = F.softmax_cross_entropy(
+                y_dis, chainer.Variable(xp.ones(batchsize, dtype=numpy.int32)))
+
+            images = train[perm[i:i + batchsize]]
+            if args.gpu >= 0:
+                images = chainer.cuda.to_gpu(images)
+            y_dis = dis(chainer.Variable(images))
+            loss_dis += F.softmax_cross_entropy(
+                y_dis, chainer.Variable(xp.zeros(batchsize, dtype=numpy.int32)))
+
+            optimizer_gen.zero_grads()
+            loss_gen.backward()
+            optimizer_gen.update()
+
+            optimizer_dis.zero_grads()
+            loss_dis.backward()
+            optimizer_dis.update()
+
+            sum_loss_gen += loss_gen.data.get()
+            sum_loss_dis += loss_dis.data.get()
+
+        if (epoch + 1) % snapshot_interval == 0:
+            outdir = '{}/{}'.format(model_dir, epoch + 1)
+            try:
+                os.makedirs(outdir)
+            except:
+                pass
+            chainer.serializers.save_npz(
+                '{}/dcgan_model_gen.npz'.format(outdir), gen)
+            chainer.serializers.save_npz(
+                '{}/dcgan_model_dis.npz'.format(outdir), dis)
+            chainer.serializers.save_npz(
+                '{}/dcgan_optimizer_gen.npz'.format(outdir), optimizer_gen)
+            chainer.serializers.save_npz(
+                '{}/dcgan_optimizer_dis.npz'.format(outdir), optimizer_dis)
