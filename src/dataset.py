6c3b4867f388d2b16645bb0881ead95b3fd2238c
Hayato Kawai <fohte.hk@gmail.com>
2016-11-28 16:08:48 +0900

    Add dataset.py

---
+++ src/dataset.py
@@ -0,0 +1,44 @@
+import argparse
+import glob
+import os
+
+import numpy
+import PIL.Image
+
+
+def convert(files, size=(64, 64)):
+    include_exts = ['.jpg', '.jpeg', '.gif', '.png']
+    files = [f for f in files if os.path.splitext(f)[1] in include_exts]
+    images = numpy.empty(
+        (len(files), 3, size[0], size[1]), dtype=numpy.float32)
+
+    for i, f in enumerate(files):
+        img = PIL.Image.open(f).convert('RGB').resize(size)
+        img = numpy.asarray(img).astype(numpy.float32).transpose(2, 0, 1)
+        images[i] = img
+
+    return images
+
+
+def load(filename):
+    images = numpy.load(filename)['img']
+    return images
+
+
+def save(filename, images):
+    numpy.savez(filename, img=images)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_dir')
+    parser.add_argument('--output', '-o', default='dataset.npz')
+    args = parser.parse_args()
+
+    files = glob.iglob(os.path.join(args.input_dir, '*'))
+    images = convert(files)
+    save(args.output, images)
+
+
+if __name__ == '__main__':
+    main()
