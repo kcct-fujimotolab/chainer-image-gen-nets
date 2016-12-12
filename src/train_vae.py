f4fff2c0559ee2fbc0aae295e47d0d0fcca8be5d
Hayato Kawai <fohte.hk@gmail.com>
2016-12-12 17:40:11 +0900

    Add VAE

---
+++ src/train_vae.py
@@ -0,0 +1,62 @@
+import argparse
+
+import chainer
+from chainer.training import extensions
+
+import dataset
+import vae.net
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Training with VAE')
+    parser.add_argument('dataset', help='The npz formatted dataset file')
+    parser.add_argument('--batchsize', '-b', type=int, default=100,
+                        help='Number of images in each mini-batch')
+    parser.add_argument('--epoch', '-e', type=int, default=1000,
+                        help='Number of sweeps over the dataset to train')
+    parser.add_argument('--gpu', '-g', type=int, default=-1,
+                        help='GPU ID (negative value indicates CPU)')
+    parser.add_argument('--out', '-o', default='result',
+                        help='Directory to output the result')
+    # parser.add_argument('--resume', '-r', default='',
+    #                     help='Resume the training from snapshot')
+    # parser.add_argument('--unit', '-u', type=int, default=1000,
+    #                     help='Number of units')
+    # parser.add_argument('--snapshot', type=int, nargs='*',
+    #                     default=range(1, 10001, 10))
+    # parser.add_argument('--filename', default='{epoch}.png')
+    parser.add_argument('--dimz', '-z', type=int, default=20,
+                        help='dimention of encoded vector')
+    args = parser.parse_args()
+
+    print('GPU: {}'.format(args.gpu))
+    print('# Minibatch-size: {}'.format(args.batchsize))
+    print('# epoch: {}'.format(args.epoch))
+    print('')
+
+    train = dataset.load(args.dataset, ndim=1)
+    train_iter = chainer.iterators.SerialIterator(train, args.batchsize)
+    img_size = train.shape[2]
+
+    model = vae.net.VAE(img_size**2, args.dimz, 500)
+    if args.gpu >= 0:
+        chainer.cuda.get_device(args.gpu).use()
+        model.to_gpu()
+
+    optimizer = chainer.optimizers.Adam()
+    optimizer.setup(model)
+
+    updater = chainer.training.StandardUpdater(
+        train_iter, optimizer, device=args.gpu)
+    trainer = chainer.training.Trainer(
+        updater, (args.epoch, 'epoch'), out=args.out)
+
+    trainer.extend(extensions.LogReport())
+    trainer.extend(extensions.PrintReport(['epoch', 'main/loss']))
+    trainer.extend(extensions.ProgressBar())
+
+    trainer.run()
+
+
+if __name__ == '__main__':
+    main()
